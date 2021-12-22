131807e4d2e71926d485cf8a8913eaf0047096c6
Tyler Johnson <tyler@csdconsultants.us>
2021-12-22 13:41:18 -0700

    adds updatePosts route with an id param

--- routes/postNotes.js
+++ routes/postNotes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const getPosts = require('../controllers/getPosts.js');
 const createPosts = require('../controllers/createPosts.js');
+const updatePosts = require('../controllers/updatePosts.js');
 
 
 const router = express.Router();
 
 router.get('/', getPosts );
 router.post('/', createPosts )
+router.patch('/:id', updatePost)
 
 module.exports = router;
