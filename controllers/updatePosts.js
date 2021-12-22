f3cf551db06df8d253414effdcc11708ddb6d395
Tyler Johnson <tyler@csdconsultants.us>
2021-12-22 13:40:58 -0700

    creates logic for updatePosts route

---
+++ controllers/updatePosts.js
@@ -0,0 +1,13 @@
+import { Mongoose } from "mongoose";
+import PostMessage from "../models/postNoteMessage";
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatedPost);
+}
