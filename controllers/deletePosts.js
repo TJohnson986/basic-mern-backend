4e02afef441a6d56e22c71f3b0eb32ee283dddf6
Tyler Johnson <tyler@csdconsultants.us>
2021-12-23 10:22:48 -0700

    corrects mongoose validation parameter typo

--- controllers/deletePosts.js
+++ controllers/deletePosts.js
@@ -1,13 +1,14 @@
 const PostMessage = require("../models/postNoteMessage");
+const mongoose = require('mongoose');
 
-const deletePost = async (req, res) => {
+const deletePosts = async (req, res) => {
     const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     await PostMessage.findByIdAndRemove(id);
 
     res.json({ message: 'Post deleted successfully' })
 }
 
-module.exports = deletePost;
+module.exports = deletePosts;
